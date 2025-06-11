6e9dddb8635b827274ebdeea8abfeafaf36d88dc
Bueno-m <matheusbuenorocha@gmail.com>
2025-06-11 16:07:34 -0300

    Add files via upload

---
+++ classe-heranca/script.js
@@ -0,0 +1,24 @@
+//Superclasse
+class Animal{
+
+    //Atributo
+    constructor(nome){
+        this.nome = nome;
+    }
+    //Método
+    fazerSom(){
+        console.log("O animal fez um som");
+    }
+}
+
+//Subclasse
+class Cachorro extends Animal{
+    //Herdando o método da Superclasse e sobreescrevendo
+    fazerSom(){
+        console.log("O cachorro late")
+    }
+}
+
+let rex = new Cachorro("Rex");
+console.log(rex.nome);
+rex.fazerSom();
